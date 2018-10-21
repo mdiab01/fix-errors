97dcbdce8d7c2342f26d3f8b8cda283eed17b5dc
Mahmud Diab <mahmud.d458@gmail.com>
2018-10-21 18:05:32 +0000

    also deleted dot after names in line 3

--- lesson4.js
+++ lesson4.js
@@ -1,5 +1,5 @@
 var names = [ "John", "Alex", "Mike", "Roger" ]
 for ( var i = 0; i < names.length; i++ ){
-    console.log("My Name is " + names.[i])
+    console.log("My Name is " + names[i])
 }
 //Should print out all names in console.
