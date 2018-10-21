17f1482b06313bce9569d6b3c861acf654bad933
Mahmud Diab <mahmud.d458@gmail.com>
2018-10-21 18:03:10 +0000

    changed commas to semicolons in line 2

--- lesson3.js
+++ lesson3.js
@@ -1,6 +1,5 @@
-var names = [ "John", "Alex", "Mike", "Roger" ]
-
-for ( var i = 0; i < names.length; i ){
-    console.log("My Name is " + names.l)
+var names = [ "John", "Alex", "Mike", "Roger" ];
+for (var i = 0; i < names.length; i++){
+    console.log("My Name is " + names[i]);
 }
 // ctrl c to stop terminal
