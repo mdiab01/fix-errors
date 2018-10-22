301984dad2e1af39e759aa658dcd13e3967d7955
Mahmud Diab <mahmud.d458@gmail.com>
2018-10-22 17:53:43 +0000

    invoked the anonymousFunction and passed in i in line 24. changed the function parameter in line 31 to num to track count.

--- lesson16.js
+++ lesson16.js
@@ -21,14 +21,14 @@ var nameFilter = function(filteredName, anonymousFunction) {
         let currentName = people[i].name
         if (currentName != filteredName) {
             results.push(currentName)
-
+            anonymousFunction(i)
         }
     }
 
     return results
 }
 
-var filteredNames = nameFilter("Betty", function(name) {
-    console.log("This Ran")
+var filteredNames = nameFilter("Betty", function(num) {
+    console.log("This Ran" + num)
 })
 console.log(filteredNames)
