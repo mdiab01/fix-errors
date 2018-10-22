672e39a0db00fecee71267c36cd4228458888a63
Mahmud Diab <mahmud.d458@gmail.com>
2018-10-22 17:19:30 +0000

    invoked the anonymousFunction in line 21

--- lesson15.js
+++ lesson15.js
@@ -9,22 +9,24 @@ var people = [
     { name: "John", age: 55 },
     { name: "Roger", age: 44 },
     { name: "Betty", age: 37 }
-    ]
+    ];
 
 
 var nameFilter = function(filteredName, anonymousFunction) {
-    var results = []
+    var results = [];
     for (var i = 0; i < people.length; i++) {
-        let currentName = people[i].name
+        let currentName = people[i].name;
         if (currentName != filteredName) {
-            results.push(currentName)
+            results.push(currentName);
+            anonymousFunction()
         }
     }
 
-    return results
-}
+    return results;
+};
 
 var filteredNames = nameFilter("Betty", function(name) {
-    console.log("This Ran")
-})
-console.log(filteredNames)
+    console.log("This Ran");
+});
+console.log(filteredNames);
+
